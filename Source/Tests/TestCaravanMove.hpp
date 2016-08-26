7bf1de2d166e6c791e7e39dbe959bedc15f403e5
Konstant <ekit.mail@gmail.com>
2016-08-26 17:36:34 +0200

    CaravanMove migrated.

---
+++ Source/Tests/TestCaravanMove.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <Urho3D/Core/CoreEvents.h>
+#include <Urho3D/Engine/Application.h>
+#include <Urho3D/Engine/Engine.h>
+#include <Urho3D/Input/Input.h>
+#include <Urho3D/Container/Vector.h>
+#include <StatesEngine/StateObject.hpp>
+
+class TestCaravanMove : public Urho3D::Application
+{
+URHO3D_OBJECT (TestCaravanMove, Urho3D::Application)
+public:
+    TestCaravanMove (Urho3D::Context *context);
+    virtual void Setup ();
+    virtual void Start ();
+    virtual void Stop ();
+    virtual ~TestCaravanMove();
+};
